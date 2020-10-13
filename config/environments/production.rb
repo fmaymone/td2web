78648cfe5eeb063c0e3265f76abff634ceeb6529
Patrick Morgan <patrick@patrick-morgan.net>
2020-10-13 16:08:14 -0700

    Config tweak and new translations

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,7 +112,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: Rails.application.credentials.config[:smtp][:address],
