612b47b03afda158e1a8cc6841989f9410bfdeb3
Patrick Morgan <patrick@patrick-morgan.net>
2021-01-14 19:49:01 -0800

    Do not require consent to logout or login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :current_tenant
   before_action :current_locale
-  before_action :authorize_consent!
+  before_action :authorize_consent!, unless: :devise_controller?
   before_action :set_page
   around_action :user_timezone, if: :current_user
 
