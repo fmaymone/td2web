1d1ddf71ab248651829e2913d37de06309162d25
Patrick Morgan <patrick@patrick-morgan.net>
2022-01-09 13:45:59 -0800

    stake

--- spec/services/team_diagnostic_services/reporter_spec.rb
+++ spec/services/team_diagnostic_services/reporter_spec.rb
@@ -12,5 +12,25 @@ RSpec.describe TeamDiagnosticServices::Reporter do
       service = TeamDiagnosticServices::Reporter.new(team_diagnostic)
       expect(service.team_diagnostic).to eq(team_diagnostic)
     end
+    it 'throws an error if a team diagnostic is not provided' do
+      expect do
+        service = TeamDiagnosticServices::Reporter.new(true)
+      end.to raise_error
+    end
+  end
+
+  describe 'calling the service' do
+    let(:service) { service = TeamDiagnosticServices::Reporter.new(team_diagnostic) }
+    describe 'with options'
+    describe 'without page options' do
+      it 'generates a report' do
+        Report.destroy_all
+        report_count = Report.count
+        service.call
+        expect(Report.count).to eq(report_count + 1)
+        binding.pry; true
+      end
+      it 'cancels all existing/running reports before running a new report'
+    end
   end
 end
