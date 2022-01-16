092538be2a6a31b93366ef7ec40c822dda57392e
Patrick Morgan <patrick@patrick-morgan.net>
2022-01-16 12:27:27 -0800

    stake

--- config/initializers/url_options.rb
+++ config/initializers/url_options.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-
 Rails.application.reloader.to_prepare do
   application_host_and_port = ENV.fetch('APPLICATION_HOST', 'localhost:3000')
   Rails.application.routes.default_url_options[:host] = application_host_and_port
