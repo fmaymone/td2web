fd4bb6f52d4290a8bb063dbb1e2763225fd353d9
Patrick Morgan <patrick@patrick-morgan.net>
2022-01-16 12:23:48 -0800

    stake

--- db/migrate/20210517231742_add_weight_to_diagnostic_questions.rb
+++ db/migrate/20210517231742_add_weight_to_diagnostic_questions.rb
@@ -1,7 +1,9 @@
 class AddWeightToDiagnosticQuestions < ActiveRecord::Migration[6.1]
   def change
     add_column :diagnostic_questions, :weight, :decimal, default: 0.0, null: false
-    #remove_index :diagnostic_questions, name: :general_idx
+    if index_exists? :diagnostic_questions, 'general_idx'
+      remove_index :diagnostic_questions, 'general_idx'
+    end
     add_index :diagnostic_questions, [:active, :category, :question_type, :factor, :matrix, :weight], name: 'general_idx'
   end
 end
