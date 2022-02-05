8c365a01525a33db16c20222c070817c611f2e44
Patrick Morgan <patrick@patrick-morgan.net>
2022-02-05 13:01:13 -0800

    added report pdf generator page layout options

--- app/services/report_services/renderers/pdf.rb
+++ app/services/report_services/renderers/pdf.rb
@@ -22,6 +22,7 @@ module ReportServices
       end
 
       def build_for_locale(locale)
+        configure_generator_for_locale(locale)
         filename = "#{report.team_diagnostic.name}-#{title}---#{locale}---#{Time.now.strftime('%Y%m%d%H%M')}.pdf"
         pdf_data = StringIO.new(generate(locale))
         @report.report_files.attach(io: pdf_data, filename:, content_type: CONTENT_TYPE)
@@ -41,6 +42,23 @@ module ReportServices
 
         pdf_service = PDFKit.new(html_data)
         pdf_service.to_pdf
+      end
+
+      private
+
+      def configure_generator_for_locale(locale)
+        page_size = ( locale.to_s == 'en' ? 'letter' : 'A4' )
+        PDFKit.configure do |config|
+          config.default_options = {
+            page_size: page_size,
+            orientation: 'Landscape',
+            print_media_type: true,
+            margin_top: '0.1cm',
+            margin_bottom: '0.1cm',
+            margin_left: '0.1cm',
+            margin_right: '0.1cm' 
+          }
+        end
       end
     end
   end
