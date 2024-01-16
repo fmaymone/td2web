534bcd073c286d6826a7bab9efd195a1d414e777
Fernando Maymone <fmaymone@gmail.com>
2024-01-16 14:57:32 -0300

    ADD error rescue

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -3,10 +3,20 @@
 # Entitlement Invitation Mailer
 class InvitationMailer < ApplicationMailer
   def entitlement_invitation(invitation)
+    puts "Sending an entitlement_invitation to #{invitation.email}"
+
     @invitation = invitation
     @email = @invitation.email
     @content = @invitation.email_body
     @subject = @invitation.email_subject
+
     mail(to: @email, subject: @subject)
+  rescue StandardError => e
+    Rails.logger.error("Error sending entitlement invitation: #{e.message}")
+    # You can also log the backtrace for more detailed information
+    Rails.logger.error(e.backtrace.join("\n"))
+    # You might want to notify yourself or the development team
+    # about the error, for example, using an error tracking service.
+    # Example: Bugsnag.notify(e)
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  # domain: 'team-coaching-ba8fa90a2744.herokuapp.com',
+  domain: 'localhost:3000',
+  user_name: 'apikey',
+  password: ENV.fetch('SENDGRID_API_KEY', nil),
+  authentication: :plain,
+  enable_starttls_auto: true
+}
